ba2134056ed4c40545524821b143325d2dc1b77e
nohsangwoo <nsgr12@naver.com>
2021-02-02 06:18:11 +0900

    주식 자동 매매 프로그램 with python

---
+++ readme.md
@@ -0,0 +1,14 @@
+# 변동성 돌파전략
+
+https://finance.daum.net/domestic/etf
+여기서 etf종목 고르기
+
+골라서 리스트에 추가
+
+# 크레온 플러스 자동실행
+
+python AutoConnect.py
+
+# 자동 매매 실행
+
+python AutoTrade.py
---
+++ test.py
@@ -0,0 +1,31 @@
+import win32com.client
+ 
+# 연결 여부 체크
+objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
+bConnect = objCpCybos.IsConnect
+if (bConnect == 0):
+    print("PLUS가 정상적으로 연결되지 않음. ")
+    exit()
+ 
+# 현재가 객체 구하기
+objStockMst = win32com.client.Dispatch("DsCbo1.StockMst")
+objStockMst.SetInputValue(0, 'A005930')   #종목 코드 - 삼성전자
+objStockMst.BlockRequest()
+ 
+# 현재가 통신 및 통신 에러 처리 
+rqStatus = objStockMst.GetDibStatus()
+rqRet = objStockMst.GetDibMsg1()
+print("통신상태", rqStatus, rqRet)
+if rqStatus != 0:
+    exit()
+ 
+# 현재가 정보 조회
+offer = objStockMst.GetHeaderValue(16)  #매도호가
+
+
+from slacker import Slacker
+
+slack = Slacker('slackCode')
+
+# Send a message to #general channel
+slack.chat.post_message('#stock', '삼성전자 현재가: ' + str(offer) )
